cfce407a7c0e9c898870990bab450fb5d1c20087
Philip Kershaw <pgckershaw@gmail.com>
2025-01-10 13:21:42 +0000

    Add: Fixed indentation on post method enabling commenting

--- blog/views.py
+++ blog/views.py
@@ -31,12 +31,12 @@ def post_detail(request, slug):
     comment_count = post.comments.filter(approved=True).count()
     
     if request.method == "POST":
-    comment_form = CommentForm(data=request.POST)
-    if comment_form.is_valid():
-        comment = comment_form.save(commit=False)
-        comment.author = request.user
-        comment.post = post
-        comment.save()
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
     
     comment_form = CommentForm()
 
