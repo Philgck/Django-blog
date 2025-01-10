9c4698cfbf6edc080dc360e591124acbc6808db2
Philip Kershaw <pgckershaw@gmail.com>
2025-01-10 16:14:26 +0000

    Add: comments.js, bugfixes, ability to edit comments

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
 from . import views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.post_detail, name="post_detail"),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
     path('<slug:slug>/edit_comment/<int:comment_id>',
-        views.comment_edit, name='comment_edit'),
+         views.comment_edit, name='comment_edit'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
