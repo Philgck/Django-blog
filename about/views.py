8d09b40cb1c4658cd1b172de5a64b7c028a1082e
Philip Kershaw <pgckershaw@gmail.com>
2025-01-10 15:20:57 +0000

    Add: Comment.js to static folder

--- about/views.py
+++ about/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
@@ -12,6 +14,16 @@ def about_me(request):
     about = About.objects.all().order_by('-updated_on').first()
     collaborate_form = CollaborateForm()
 
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                "Collaboration request received! I endeavour to respond within 2 working days.",
+            )
     return render(
         request,
         "about/about.html",
